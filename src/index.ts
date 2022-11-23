3ba79b6a60286bcb73636aa67afd8b90457edbb4
Diego <diegoprudencioo@gmail.com>
2022-11-22 22:59:21 -0300

    :tada: init: main: primeiro commit com add de HTML e CSS.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+console.log("Olá TypeScript");
+
+//Tipos primitivos:
+//string, number, boolean
+var nome: string = "Diego";
+var idade: number = 10;
+
+const isAluno: boolean = true;
+
+//Tipos Object
+//Array
