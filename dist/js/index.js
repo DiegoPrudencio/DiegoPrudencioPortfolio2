7f3151fc012a103f61ea90a5015046a185186271
Diego <diegoprudencioo@gmail.com>
2022-12-09 00:19:31 -0300

    :books: docs: main: modificação total do  html e css.

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,19 +1,54 @@
 "use strict";
-var btn = document.getElementById("botao");
+var btn = document.getElementById("idBotao");
+var check = document.getElementById("idcheck");
 var btn2 = document.getElementById("atu");
-var theme = window.localStorage.getItem("theme");
-var logoImage1 = document.getElementById("imageLogo1");
-if (theme === "dark")
-    document.body.classList.add("dark");
-btn.addEventListener("click", () => {
+// var theme = window.localStorage.getItem("theme");
+var imageLogoTop1 = document.getElementById("imageLogoTop1");
+var imageLogoTop2 = document.getElementById("imageLogoTop2");
+var imageLinkedin1 = document.getElementById("imageLinkedin1");
+var imageLinkedin2 = document.getElementById("imageLinkedin2");
+// if (theme === "dark") document.body.classList.add("dark");
+check.addEventListener("click", () => {
     document.body.classList.toggle("dark");
-    if (theme === "dark") {
-        window.localStorage.setItem("theme", "light");
-    }
-    else
-        window.localStorage.setItem("theme", "dark");
-    logoImage1.hidden = true;
+    imageLogoTop1.hidden = true;
+    imageLogoTop2.hidden = false;
+    imageLinkedin1.hidden = true;
+    imageLinkedin2.hidden = false;
+    // if (check.checked == true) {
+    // if (check.checked = false) 
+    // else {
+    //     document.body.classList.toggle("light");
+    //     imageLogoTop1.hidden = false;
+    //     imageLogoTop2.hidden = true;
+    //     imageLinkedin1.hidden = false;
+    //     imageLinkedin2.hidden = true;
+    // }
+    // if (theme === "dark") {
+    //     window.localStorage.setItem("theme", "light");
+    //   } else window.localStorage.setItem("theme", "dark");
+    // while (document.body.classList.contains("dark")) {
+    //     let darkMode = 'on'
+    //     changeImage(darkMode);
+    // }
+    // if () {
+    //     let darkMode = 'on'
+    //     changeImage(darkMode);
+    // }
 });
 btn2.addEventListener("click", () => {
     window.location.reload();
 });
+function changeImage(darkMode) {
+    if (darkMode = 'on') {
+        imageLogoTop1.hidden = true;
+        imageLogoTop2.hidden = false;
+        imageLinkedin1.hidden = true;
+        imageLinkedin2.hidden = false;
+    }
+    else {
+        imageLogoTop1.hidden = false;
+        imageLogoTop2.hidden = true;
+        imageLinkedin1.hidden = false;
+        imageLinkedin2.hidden = true;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,69 @@
-var btn = document.getElementById("botao") as HTMLButtonElement;
+var btn = document.getElementById("idBotao") as HTMLButtonElement;
+var check = document.getElementById("idcheck") as HTMLInputElement;
 var btn2 = document.getElementById("atu") as HTMLButtonElement;
-var theme = window.localStorage.getItem("theme");
-var logoImage1 = document.getElementById("imageLogo1") as HTMLImageElement;
+// var theme = window.localStorage.getItem("theme");
+var imageLogoTop1 = document.getElementById("imageLogoTop1") as HTMLImageElement;
+var imageLogoTop2 = document.getElementById("imageLogoTop2") as HTMLImageElement;
+var imageLinkedin1 = document.getElementById("imageLinkedin1") as HTMLImageElement;
+var imageLinkedin2 = document.getElementById("imageLinkedin2") as HTMLImageElement;
 
-if (theme === "dark") document.body.classList.add("dark");
+// if (theme === "dark") document.body.classList.add("dark");
 
-btn.addEventListener("click", () => {
+check.addEventListener("click", () => {
+
     document.body.classList.toggle("dark");
-    if (theme === "dark") {
-        window.localStorage.setItem("theme", "light");
-      } else window.localStorage.setItem("theme", "dark");
-    logoImage1.hidden = true
+    imageLogoTop1.hidden = true;
+    imageLogoTop2.hidden = false;
+    imageLinkedin1.hidden = true;
+    imageLinkedin2.hidden = false;
+
+
+    // if (check.checked == true) {
+
+    // if (check.checked = false) 
+    // else {
+    //     document.body.classList.toggle("light");
+    //     imageLogoTop1.hidden = false;
+    //     imageLogoTop2.hidden = true;
+    //     imageLinkedin1.hidden = false;
+    //     imageLinkedin2.hidden = true;
+
+    // }
+
+    // if (theme === "dark") {
+    //     window.localStorage.setItem("theme", "light");
+    //   } else window.localStorage.setItem("theme", "dark");
+
+    // while (document.body.classList.contains("dark")) {
+    //     let darkMode = 'on'
+    //     changeImage(darkMode);
+    // }
+
+    // if () {
+    //     let darkMode = 'on'
+    //     changeImage(darkMode);
+    // }
+
 });
 
 btn2.addEventListener("click", () => {
     window.location.reload();
 });
+
+
+function changeImage(darkMode: string) {
+
+    if (darkMode = 'on') {
+        imageLogoTop1.hidden = true;
+        imageLogoTop2.hidden = false;
+        imageLinkedin1.hidden = true;
+        imageLinkedin2.hidden = false;
+
+    } else {
+        imageLogoTop1.hidden = false;
+        imageLogoTop2.hidden = true;
+        imageLinkedin1.hidden = false;
+        imageLinkedin2.hidden = true;
+    }
+
+}
